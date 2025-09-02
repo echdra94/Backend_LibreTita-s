02d5fa0a2db30d9db57c68c9c231fa48c5993c67
Stefani Cosío <stefanicosio@hotmail.com>
2025-09-02 17:52:08 -0600

    Se añade un registro a la BD

--- BD/Script_Tablas_LibreTitas.sql
+++ BD/Script_Tablas_LibreTitas.sql
@@ -10,7 +10,8 @@ VALUES (null, "Alejandra", "Echavarria", "alejandraegib@gmail.com", "Alejandra12
         (null, "Mariell", "Zamora", "zamoramariell29@gmail.com", "MarZamora29!", 5581808909, 1),
         (null, "Marisol", "Díaz", "jimenezdiazm158@gmail.com", "Mary2322.", 5566173426, 0),
         (null, "Anya" , "Mares" , "anyamares@gmail.com" , "anyvany" , 3317796365, 0),
-        (null, "Ana", "Guevara", "anaguevaraaguilar37@.gmail.com", "Ana12!", 5551234567, 0);
+        (null, "Ana", "Guevara", "anaguevaraaguilar37@gmail.com", "Ana12!", 5551234567, 0),
+		(null, "Stefani", "Cosío", "stefanicos@gmail.com", "Stefani123$", 5580334144, 0);
 
  -- Modificación tipo de variable de decripción de 100 a 150: VARCHAR(150)
  ALTER TABLE producto
